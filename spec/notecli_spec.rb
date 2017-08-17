6f748cdac8b5bdac3e13fd39a8dde9c923f7dab6
zpallin <zpallin@reputation.com>
2017-08-16 17:02:30 -0700

    Add some spec tests for CLI: groups command

--- spec/notecli_spec.rb
+++ spec/notecli_spec.rb
@@ -1,12 +1,43 @@
+
 require "spec_helper"
+require "pp"
+require "fakefs"
+require "notecli/cli"
+require "notecli/version"
+require "notecli/page"
+require "notecli/group"
+require "notecli/config"
+
+Link = Notecli::Link
+CLI = Notecli::CLI
 
 RSpec.describe Notecli do
   it "has a version number" do
     expect(Notecli::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(false)
+  describe CLI do
+    context "groups" do
+      let(:output_find) {capture(:stdout) {subject.find}}
+      it "can list all of the current groups" do
+        FakeFS do
+          Group.new "test1"
+          Group.new "test2"
+          data = capture(:stdout) { subject.groups }
+          expect(data).to eq(
+            "list all groupings with match \".*\"\n---\n- test1\n- test2\n")
+        end
+      end
+      it "can match groupings and list them" do
+        FakeFS do 
+          Group.new "test1"
+          Group.new "test2"
+          data = capture(:stdout) { subject.groups "test*" }
+          expect(data).to eq(
+            "list all groupings with match \"test*\"\n---\n- test1\n- test2\n")
+        end
+      end
+    end
   end
 end
 
